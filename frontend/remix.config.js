19c0cdc87342593812824ce8aaa43bb5b08471a9
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2023-10-29 14:18:45 +0100

    remix, shadcn, microservices?

---
+++ frontend/remix.config.js
@@ -0,0 +1,10 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  ignoredRouteFiles: ["**/.*"],
+  // appDirectory: "app",
+  tailwind: true,
+  postcss: true,
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
+  // serverBuildPath: "build/index.js",
+};
